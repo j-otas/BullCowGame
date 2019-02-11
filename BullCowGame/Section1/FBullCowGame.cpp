8e910d194d8449cdbd9006cea10bcf0cf0dddfd2
grunes <39992649+j-otas@users.noreply.github.com>
2019-02-11 19:57:06 +0800

    Add files via upload

---
+++ BullCowGame/Section1/FBullCowGame.cpp
@@ -0,0 +1,94 @@
+#pragma once
+
+#include "FBullCowGame.h"
+
+// to make syntax Unreal friendly
+using int32 = int;
+
+FBullCowGame::FBullCowGame() { Reset(); }
+
+int32 FBullCowGame::GetMaxTries() const { return ; }
+int32 FBullCowGame::GetCurrentTry() const { return MyCurrentTry; }
+int32 FBullCowGame::GetHiddenWordLength() const { return MyHiddenWord.length(); }
+
+void FBullCowGame::Reset()
+{
+	constexpr int32 MAX_TRIES = 8;
+	const FString HIDDEN_WORD = "planet";
+
+	MyMaxTries = MAX_TRIES;
+	MyHiddenWord = HIDDEN_WORD;
+	MyCurrentTry = 1;
+
+	return;
+}
+
+
+EGuessStatus FBullCowGame::CheckGuessValidity(FString Guess) const
+{
+	if (!IsIsogram(Guess)) // if the guess isn't an isogram
+	{
+		return EGuessStatus::Not_Isogram;
+	}
+	else if (!IsLowercase(Guess)) // if the guess isn't all lowercase
+	{
+		return EGuessStatus::Not_Lowercase;
+	}
+	else if (Guess.length() != GetHiddenWordLength()) // if the guess length is wrong
+	{
+		return EGuessStatus::Wrong_Length;
+	}
+	else
+	{
+		return EGuessStatus::OK;
+	}
+}
+
+
+// receives a VALID guess, incriments turn, and returns count
+FBullCowCount FBullCowGame::SubmitValidGuess(FString Guess)
+{
+	MyCurrentTry++;
+	FBullCowCount BullCowCount;
+	int32 WordLength = MyHiddenWord.length(); // assuming same length as guess
+
+	// loop through all letters in the hidden word
+	for (int32 MHWChar = 0; MHWChar < WordLength; MHWChar++)
+	{
+		// compare letters against the guess
+		for (int32 GChar = 0; GChar < WordLength; GChar++)
+		{
+			// if they match then
+			if (Guess[GChar] == MyHiddenWord[MHWChar])
+			{
+				if (MHWChar == GChar)
+				{ // if they're in the same place
+					BullCowCount.Bulls++; // incriment bulls
+				}
+				else
+				{
+					BullCowCount.Cows++; // must be a cow
+				}
+			}
+		}
+	}
+	if (BullCowCount.Bulls == WordLength)
+	{
+		bGameIsWon = true;
+	}
+	else
+	{
+		bGameIsWon = false;
+	}
+	return BullCowCount;
+}
+
+bool FBullCowGame::IsIsogram(FString Word) const
+{
+	return true; // for example in cases where /0 is entered
+}
+
+bool FBullCowGame::IsLowercase(FString Word) const
+{
+	return true;
+}
